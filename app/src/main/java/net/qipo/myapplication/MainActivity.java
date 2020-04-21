0d14861653cf831a2e3a90f93e5567739aaa291c
key <mail@loong.io>
2020-04-21 22:05:36 +0800

    update

--- app/src/main/java/net/qipo/myapplication/MainActivity.java
+++ app/src/main/java/net/qipo/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package net.qipo.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 打印日志
         Log.d(TAG, "onCreate execute");
+
+
         // 在活动中，可以通过findViewById获得到在布局文件中定义的元素,获得Button的实例
         Button button1 = findViewById(R.id.button1);
         // 我们可以在实例上调用该方法为实例注册一个监听器
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 销毁当前活动
         Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 显式活动打开一个活动
         Button button4 = findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +54,59 @@ public class MainActivity extends AppCompatActivity {
                 // Activity类中提供了一个Activity方法，这个方法专门用于启动活动，它接受一个Intent参数
                 startActivity(intent);
             }
+        });
+
+        // 隐式Intent打开一个活动
+        Button button5 = findViewById(R.id.button5);
+        button5.setOnClickListener((v)-> {
+            // 我们使用了Intent的另一个构造函数，直接将action的字符串传了进去，表明我们想要启动能够响应这个字符串的活动。
+            // 我们之前在xml中指定的是默认的category，在调用startActivity方法的时候会自动将这个category添加到Intent中
+            Intent intent = new Intent("net.qipo.myapplication.ACTION_START");
+            // 如果xml中不包含net.qipo.myapplication.MY_CATEGORY,程序就会崩溃，没有任何一个活动可以响应我们的Intent
+            // 一个活动只能指定一个action,但是可以包含多个category，将其加上就可以响应
+            intent.addCategory("net.qipo.myapplication.MY_CATEGORY");
+            startActivity(intent);
+        });
+
+        // 打开一个网页
+        Button button6 = findViewById(R.id.button6);
+        button6.setOnClickListener((v) -> {
+            // Intent.ACTION_VIEW，这是一个Android系统的内置动作，其常量值为android.intent.action.VIEW
+            // 通过Uri.parse()方法将一个字符串解析为Uri对象
+            // 再调用Intent的setData方法将这个Uri对象传递进去
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            // 这个方法接受一个Uri对象，主要用于指定当前Intent正在操作的数据。
+            intent.setData(Uri.parse("http://baidu.com"));
+            startActivity(intent);
+        });
+
+        // 拨打电话
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener((v) -> {
+            // 除了http协议，我们可以指定很多其他协议，比如geo标识地理位置，tel标识拨打电话
+            // Intent.ACTION_DIAL，这是android系统的内置动作
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        });
+
+        // 向下一个活动传递数据
+        Button button8 = findViewById(R.id.button8);
+        button8.setOnClickListener((v) -> {
+            String data = "hello world";
+            // 通过putExtra方法传递一个字符串，注意这里putExtra接收两个参数，第一个参数是键，第二个参数才是真正要传递的数据
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("extra_data", data);
+            startActivity(intent);
+
+        });
+
+        // 返回数据给上一个活动
+        Button button9 = findViewById(R.id.button9);
+        button9.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            // 该方法第二个参数是请求码，用于在之后的回调中判断数据的来源，请求码只要是一个唯一值就可以。
+            startActivityForResult(intent, 1);
         });
     }
 
@@ -73,5 +132,18 @@ public class MainActivity extends AppCompatActivity {
                 default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String resultedData = data.getStringExtra("data_result");
+                    Log.d(TAG, "onActivityResult: " + resultedData);
+                }
+                break;
+                default:
+        }
     }
 }
--- app/src/main/java/net/qipo/myapplication/SecondActivity.java
+++ app/src/main/java/net/qipo/myapplication/SecondActivity.java
@@ -14,13 +14,35 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Log.d(TAG, "onCreate: execute");
+        // 通过getIntent用于获取启动该活动的Intent，然后调用getStringExtra传入响应的键值就可以得到传递的数据。
+        // 这里传递的是String,所以用getStringExtra方法
+        // 如果传递的是整数数据，则使用getIntExtra方法
+        // 如果传递的是布尔型数据，则使用getBooleanExtra方法
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d(TAG, "onCreate: execute data: " + data);
+        Toast.makeText(SecondActivity.this, data, Toast.LENGTH_SHORT).show();
+
         Button button3 = findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(SecondActivity.this, "you click button3", Toast.LENGTH_SHORT).show();
             }
+        });
+
+        Button button10 = findViewById(R.id.button10);
+        button10.setOnClickListener((v) -> {
+            // 可以看到，我们还是构建了一个Intent，只不过仅仅用于传递数据。
+            // 我们把传递的数据放在了intent中，然后调用了setResult，这个方法是专门因故向上一个活动返回数据的。
+            // setResult第一个参数是用于向上一个活动返回处理结果的，一个只使用RESULT_OK或者RESULT_CANCEL
+            // 第二个参数则是把带有数据的Intent传递回去
+            // 然后调用finish()方法来销毁当前活动
+            // 被销毁之后就会回调上一个活动的onActivityResult方法，因此我们需要在MainActivity中重写这个方法来得到返回的数据
+            Intent intent1 = new Intent();
+            intent1.putExtra("data_result", "Hello MainActivity");
+            setResult(RESULT_OK, intent1);
+            finish();
         });
     }
 }
